ae0e8153c6b3d70012816dd0ae46c3de02ecb660
KKSawarn <kksawarn@gmail.com>
2021-06-15 17:42:32 +0530

    student curd operation

---
+++ fbvSerializers/fbvApp/urls.py
@@ -0,0 +1,23 @@
+"""fbvSerializers URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from fbvApp.views import student_list, student_details, home
+urlpatterns = [
+    path('/',home),
+    path('students/',student_list ),
+    path('student/<int:pk>',student_details),
+]
